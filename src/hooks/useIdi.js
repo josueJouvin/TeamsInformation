1350717a8bc788603a77ad375845dfc95e22c470
josue jouvin <josuejouvin27@gmail.com>
2023-06-12 14:32:07 -0500

    refactorizado useIdi

---
+++ src/hooks/useIdi.js
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import { informationTeam } from "../services/informationTeam";
+import { useRoute } from "wouter";
+
+export function useIdi() {
+  const [, params] = useRoute("/teamInformation/:id");
+  const id = params? params.id : ""
+  const [players, setPlayers] = useState([]);
+  const [load, setLoading] = useState(false);
+  const previusId = useRef("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id && id !== previusId.current) {
+
+        try {
+          setLoading(true);
+          previusId.current = params.id;
+          const teamPlayers = await informationTeam({id});
+          setPlayers(teamPlayers);
+        } catch (e) {
+          console.log(e);
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  return { players, load };
+}
